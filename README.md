742b26e39ff5e97d529896424fa50770240e2417
NishinomeMaki <35701101+NishinomeMaki@users.noreply.github.com>
2018-03-28 12:05:38 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# NishinomeMaki.github.io
---
+++ hw/Assignment_4/js/script.js
@@ -0,0 +1,74 @@
+$(function(){
+  //Hide stuff
+
+  $('#getStarted,#finish,#previous, .hide').hide();
+  //alert("hello");
+  $('#title').on('mouseover',function(){
+  $('.fa-arrow-alt-circle-left').toggleClass('fa-arrow-alt-circle-right');
+  $('#getStarted').show();
+ });
+
+ $('#title').on('mouseleave',function(){
+ $('.fa-arrow-alt-circle-left').toggleClass('fa-arrow-alt-circle-right');
+});
+
+$('#getStarted').on('click', function(){
+$('.hide').show();
+$('#getStarted').hide();
+});
+
+$('#finish').on('click',function(){
+  $('.hide,#next, #previous, #finish').hide();
+});
+
+//*next*//
+$('#next').on('click',function(){
+
+ var currentItem = $('li.active');
+ var nextItem = $('li.active').next();
+
+ currentItem.toggleClass('active');
+ nextItem.toggleClass('active');
+
+ if($('li').last().hasClass('active')){
+$('#next').hide();
+$('#finish').show();
+ } else{
+$('#next').show();
+}
+
+if($('li').first().hasClass('active')){
+$('#previous').hide();
+} else{
+$('#previous').show();
+
+
+
+ }
+
+});
+
+//*previous*//
+$('#previous').on('click',function(){
+
+ var currentItem = $('li.active');
+ var previousItem = $('li.active').prev();
+
+ currentItem.toggleClass('active');
+ previousItem.toggleClass('active');
+
+ if($('li').first().hasClass('active')){
+$('#previous').hide();
+ } else{
+$('#previous').show();
+ }
+
+ if($('li').last().hasClass('active')){
+$('#next').hide();
+} else{
+$('#next').show();
+}
+});
+
+
+});
